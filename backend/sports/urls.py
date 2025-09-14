d0cbdfe6010eea18b39ebf2b308ec6fab67a94ba
Taylor <taylorgswayze@gmail.com>
2025-09-13 22:48:21 -0400

    Update backend configuration and URL routing

--- backend/sports/urls.py
+++ backend/sports/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve # Import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('nfl.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    # Serve static files directly from STATIC_ROOT
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Catch-all for SPA index.html - MUST be the last entry
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+]
